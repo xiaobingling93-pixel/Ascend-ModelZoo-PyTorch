8025da04cb5b295681dbad5246cdd3eb1c41cff0
代维华 <11390941+gitee-code-template@user.noreply.gitee.com>
2024-04-07 01:52:46 +0000

    !6231 修改cogvlm模型hf推理 Merge pull request !6231 from 代维华/master

--- PyTorch/built-in/foundation/CogVLM/cogvlm_utils/code/modeling_cogvlm.py
+++ PyTorch/built-in/foundation/CogVLM/cogvlm_utils/code/modeling_cogvlm.py
@@ -109,29 +109,6 @@ def get_expert_mask(token_type_ids: "torch.LongTensor(B, L)") -> "[torch.BoolTen
     return vision_token_mask, language_token_mask
 
 
-def scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None) -> torch.Tensor:
-    # Efficient implementation equivalent to the following:
-    L, S = query.size(-2), key.size(-2)
-    scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
-    attn_bias = torch.zeros(L, S, dtype=query.dtype).to(query.device)
-    if is_causal:
-        assert attn_mask is None
-        temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0).to(query.device)
-        attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
-        attn_bias.to(query.dtype)
-
-    if attn_mask is not None:
-        if attn_mask.dtype == torch.bool:
-            attn_mask.masked_fill_(attn_mask.logical_not(), float("-inf"))
-        else:
-            attn_bias += attn_mask
-    attn_weight = query @ key.transpose(-2, -1) * scale_factor
-    attn_weight += attn_bias
-    attn_weight = torch.softmax(attn_weight, dim=-1)
-    attn_weight = torch.dropout(attn_weight, dropout_p, train=True)
-    return attn_weight @ value
-
-
 class VisionExpertMLP(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -162,7 +139,12 @@ def attention_fn(
         warnings.warn("It's recommended to use torch2.0 or higher.")
     if int(torch.__version__.split('.')[0]) >= 2 and scaling_attention_score and (is_full or is_low_triangle):
         dropout_p = 0. if attention_dropout is None or not attention_dropout.training else attention_dropout.p
-        scaled_dot_product_attention(query_layer, key_layer, value_layer, dropout_p=dropout_p, is_causal=not is_full)
+        return torch.nn.functional.scaled_dot_product_attention(
+            query_layer, key_layer, value_layer,
+            attn_mask=None,
+            dropout_p=dropout_p,
+            is_causal=not is_full
+        )
     else:
         if scaling_attention_score:
             query_layer = query_layer / math.sqrt(query_layer.shape[-1])
@@ -218,10 +200,6 @@ class VisionExpertAttention(nn.Module):
         query_states = self._transpose_for_scores(query_states)  # B, H, L, HD
         key_states = self._transpose_for_scores(key_states)  # B, H, L, HD
         value_states = self._transpose_for_scores(value_states)  # B, H, L, HD
-
-        kv_seq_len = key_states.shape[-2]
-        if past_key_value is not None:
-            kv_seq_len += past_key_value[0].shape[-2]
 
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
