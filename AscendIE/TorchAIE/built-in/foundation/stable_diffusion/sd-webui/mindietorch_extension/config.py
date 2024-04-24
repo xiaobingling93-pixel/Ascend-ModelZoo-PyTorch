52e889be6e377c2f5f3bbd09468ad50ff2cfbb3b
guowenna <guowenna1@huawei.com>
2024-04-24 12:49:02 +0000

    !6286 [自研][推理引擎AscendIE][StableDiffusion]MindIE-Torch SD代码迁移 Merge pull request !6286 from guowenna/master

--- AscendIE/TorchAIE/built-in/foundation/stable_diffusion/sd-webui/mindietorch_extension/config.py
+++
@@ -1,9 +0,0 @@
-class NpuConfig(object):
-    use_cpu = True
-    Duo = False
-    A2 = False
-    compiled_unet_model_1_5 = None
-    compiled_unet_model_2_1 = None
-    compiled_unet_model_xl = None
-
-
--- AscendIE/TorchAIE/built-in/foundation/stable_diffusion/sd-webui/onnx_extension/config.py
+++
@@ -1,9 +0,0 @@
-class NpuConfig(object):
-    use_cpu = True
-    Duo = False
-    A2 = False
-    use_parallel_inferencing = False
-    unet_session = False
-    clip_session = False
-    vae_session = False
-    unet_session_bg = False
