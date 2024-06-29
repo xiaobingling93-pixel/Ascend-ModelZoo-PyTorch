207387abe9ee85dfd9ebe07fa65651c6dbf4fa1f
孙毅 <1659275352@qq.com>
2024-06-29 01:44:22 +0000

    !6471 [built-in][Pytorch][ResNet50_ID4149] add 16p script Merge pull request !6471 from 孙毅/resnet-c

--- PyTorch/built-in/cv/classification/ResNet50_ID4149_for_PyTorch/main.py
+++ PyTorch/built-in/cv/classification/ResNet50_ID4149_for_PyTorch/main.py
@@ -127,6 +127,7 @@ parser.add_argument('--opt-level', default='O2', type=str,
 parser.add_argument('--warmup', default=8, type=int, metavar='E', help='number of warmup epochs')
 parser.add_argument('--label-smoothing', default=0.1, type=float, metavar='S', help='label smoothing')
 parser.add_argument('--addr', default='127.0.0.1', type=str, help='master addr')
+parser.add_argument('--image_size', default=224, type=int, required=False, help='size of image')
 best_acc1 = 0
 
 def lr_policy(lr_fn):
@@ -357,7 +358,7 @@ def main_worker(gpu, ngpus_per_node, args):
         train_dataset = datasets.ImageFolder(
             traindir,
             transforms.Compose([
-                transforms.RandomResizedCrop(224),
+                transforms.RandomResizedCrop(args.image_size),
                 transforms.RandomHorizontalFlip(),
             ]))
 
