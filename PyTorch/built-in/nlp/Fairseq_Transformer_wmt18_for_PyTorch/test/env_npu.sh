1ce71394052fc5438b2235be283e4f805a370d71
卓博航 <zhuobohang@huawei.com>
2024-08-29 01:21:27 +0000

    !6658 【PyTorch】【built-in】【Fairseq_Transformers】使能TASK_QUEUE_ENABLE=2 Merge pull request !6658 from 卓博航/fairseq

--- PyTorch/built-in/nlp/Fairseq_Transformer_wmt18_for_PyTorch/test/env_npu.sh
+++ PyTorch/built-in/nlp/Fairseq_Transformer_wmt18_for_PyTorch/test/env_npu.sh
@@ -19,8 +19,8 @@ export ASCEND_SLOG_PRINT_TO_STDOUT=0
 export ASCEND_GLOBAL_LOG_LEVEL=3
 #设置Event日志开启标志,0-关闭/1-开启
 export ASCEND_GLOBAL_EVENT_ENABLE=0
-#设置是否开启taskque,0-关闭/1-开启
-export TASK_QUEUE_ENABLE=1
+#设置taskque,0-关闭/1-旧taskque/2-新taskque
+export TASK_QUEUE_ENABLE=2
 #HCCL白名单开关,1-关闭/0-开启
 export HCCL_WHITELIST_DISABLE=1
 
