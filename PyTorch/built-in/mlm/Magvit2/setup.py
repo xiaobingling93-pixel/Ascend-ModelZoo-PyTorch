6034cb77483089d1e0f8a1db4486ff842b5c36ec
zzztq <ztqwdk@126.com>
2024-08-29 02:11:50 +0000

    !6654 [built-in][Pytorch][Magvit2] Submit Magvit2 source code Merge pull request !6654 from zzztq/master

---
+++ PyTorch/built-in/mlm/Magvit2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+exec(open('magvit2_pytorch/version.py').read())
+
+setup(
+  name = 'magvit2-pytorch',
+  packages = find_packages(),
+  version = __version__,
+  license='MIT',
+  description = 'MagViT2 - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/magvit2-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformer',
+    'attention mechanisms',
+    'generative video model'
+  ],
+  install_requires=[
+    'accelerate>=0.24.0',
+    'beartype',
+    'einops>=0.7.0',
+    'ema-pytorch>=0.2.4',
+    'pytorch-warmup',
+    'gateloop-transformer>=0.2.2',
+    'kornia',
+    'opencv-python',
+    'pillow',
+    'pytorch-custom-utils>=0.0.9',
+    'numpy',
+    'vector-quantize-pytorch>=1.14.39',
+    'taylor-series-linear-attention>=0.1.5',
+    'torch',
+    'torchvision',
+    'x-transformers'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
