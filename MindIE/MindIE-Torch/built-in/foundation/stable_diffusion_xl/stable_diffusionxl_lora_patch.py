5982c7b66f941d9a993de0c7c6e9b5fe7e15d6d5
李少鹏 <lishaopeng21@huawei.com>
2024-09-07 11:02:15 +0000

    !6726 新的lora热切功能补丁脚本 Merge pull request !6726 from 李少鹏/lora_newpatch

--- MindIE/MindIE-Torch/built-in/foundation/stable_diffusion_xl/stable_diffusionxl_lora_patch.py
+++ MindIE/MindIE-Torch/built-in/foundation/stable_diffusion_xl/stable_diffusionxl_lora_patch.py
@@ -24,6 +24,7 @@ def main():
     if diffusers_version != '0.26.3':
         logging.error("patch error! diffusers_version does not equal to 0.26.3")
     os.system(f'patch -p0 {diffusers_path[0]}/models/lora.py lora.patch')
+    os.system(f'patch -p0 {diffusers_path[0]}/models/attention_processor.py attention_lora.patch')
 
 
 if __name__ == '__main__':
