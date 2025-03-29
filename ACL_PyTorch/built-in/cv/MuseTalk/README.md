2fec68e9b199340f1e4ec32aad85436d29ab7071
沈鑫杰 <15306573985@163.com>
2025-03-29 09:53:22 +0000

    !7121 [Feat][ACL-PyTorch][cv][MuseTalk] 新增MuseTalk Merge pull request !7121 from 沈鑫杰/musetalk

---
+++ ACL_PyTorch/built-in/cv/MuseTalk/README.md
@@ -0,0 +1,144 @@
+# MuseTalk-推理指导
+
+- [MuseTalk-推理指导](#musetalk-推理指导)
+- [概述](#概述)
+- [推理环境准备](#推理环境准备)
+- [快速上手](#快速上手)
+  - [获取源码](#获取源码)
+
+******
+
+# 概述
+&emsp;&emsp;‌`MuseTalk` 是一个实时的音频驱动唇部同步模型。该模型能够根据输入的音频信号，自动调整数字人物的面部图像，使其唇形与音频内容高度同步。这样，观众就能看到数字人物口型与声音完美匹配的效果。MuseTalk 特别适用于256 x 256像素的面部区域，且支持中文、英文和日文等多种语言输入。
+
+- 版本说明：
+  ```
+  url=https://github.com/TMElyralab/MuseTalk
+  commit_id=
+  model_name=MuseTalk
+  ```
+
+# 推理环境准备
+- 该模型需要以下插件与驱动
+  **表 1**  版本配套表
+
+  | 配套                                                            |   版本 | 环境准备指导                                                                                          |
+  | ------------------------------------------------------------    | ------ | ------------------------------------------------------------                                          |
+  | 固件与驱动                                                       | 25.0.RC1 | [Pytorch框架推理环境准备](https://www.hiascend.com/document/detail/zh/ModelZoo/pytorchframework/pies) |
+  | CANN                                                            |  8.1.RC1 | 包含kernels包和toolkit包                                                                                                   |
+  | Python                                                          |  3.10 | -                                                                                                     |
+  | PyTorch                                                         | 2.1.0 | -                                                                                                     |
+  | Ascend Extension PyTorch                                        | 2.1.0.post10 | -                                                                                                     |
+  | 说明：Atlas 300I DUO 推理卡请以CANN版本选择实际固件与驱动版本。 |      \ | \                                                                                                     |
+
+
+# 快速上手
+
+## 获取源码
+
+1. 获取开源模型源码
+   ```
+   git clone https://github.com/TMElyralab/MuseTalk
+   cd MuseTalk
+   git reset --hard 058f7dd
+   ```
+2. 下载[ffmpeg-static](https://www.johnvansickle.com/ffmpeg/old-releases/), 并放置于path/to/ffmpeg文件夹下
+   
+
+3. 下载MuseTalk权重 [weights](https://huggingface.co/TMElyralab/MuseTalk).
+
+4. 下载其他依赖权重（根据MuseTalk开源仓资料说明下载）:
+   - [sd-vae-ft-mse，自动编码器，用于视频或图片特征的编解码](https://huggingface.co/stabilityai/sd-vae-ft-mse)
+   - [whisper，用于audio特征提取](https://openaipublic.azureedge.net/main/whisper/models/65147644a518d12f04e32d6f3b26facc3f8dd46e5390956a9424a650c0ce22b9/tiny.pt)
+   - [dwpose，用于人体姿态分析](https://huggingface.co/yzd-v/DWPose/tree/main)
+   - [face-parse-bisent，用于人脸解析](https://github.com/zllrunning/face-parsing.PyTorch)
+   - [resnet18，用于人脸解析](https://download.pytorch.org/models/resnet18-5c106cde.pth)
+   
+   
+   将下载后的权重放置于`models`文件夹下，本地下载完成后的目录树如下
+   ```shell
+    MuseTalk
+    ├── models
+    │   ├── musetalk
+    │   │   └── musetalk.json
+    │   │   └── pytorch_model.bin
+    │   ├── dwpose
+    │   │   └── dw-ll_ucoco_384.pth
+    │   ├── face-parse-bisent
+    │   │   ├── 79999_iter.pth
+    │   │   └── resnet18-5c106cde.pth
+    │   ├── sd-vae-ft-mse
+    │   │   ├── config.json
+    │   │   └── diffusion_pytorch_model.bin
+    │   └── whisper
+    │       └── tiny.pt
+    ├── musetalk
+    │   ├── models
+    │   ├── utils
+    │   └── whisper
+    ├── scripts
+    │   ├── inference.py
+    │   ├── infer_npu.py      // 本仓库提供的自定义推理脚本
+    │   ├── diff.patch        // 本仓库提供的patch文件
+    │   ├── requirements.txt  // 本仓库提供的requirements文件
+    │   ├── realtime_inference.py
+    │   └── __init__.py
+    ├── configs
+    │   └── inference
+    ├── data
+    │   ├── audio
+    │   └── video
+    ├── app.py
+    ├── entrypoint.sh
+    ├── README.md
+    ├── requirements.txt
+    └── LICENSE
+    ```
+
+
+2. 安装依赖  
+   ```
+   pip3 install scripts/requirements.txt
+   ```
+
+
+## 模型推理
+
+### 1 开始推理验证
+
+   1. 设置环境变量，执行推理命令
+
+      ```
+      # 指定使用NPU ID，默认为0
+      export ASCEND_RT_VISIBLE_DEVICES=0
+
+      # 设置ffmpeg-static环境变量
+      export FFMPEG_PATH=/path/to/ffmpeg
+
+      # 应用patch文件
+      git apply scripts/diff.patch
+
+      # 执行推理命令
+      python -m scripts.infer_npu --inference_config configs/inference/test.yaml (--use_float16)
+
+      ```
+
+      在推理开始后，首先会默认执行warm_up，目的是执行首次编译，首次编译时间较长，在warm_up结束后，会执行推理操作，并打屏计算结果和E2E性能数据。如果想测试模型推理耗时，可以在 `infer_npu.py` 文件 190行 `res_frame_list = infer(pd.input_latent_list_cycle, pd.whisper_chunks)` 前后添加时间打点。
+
+### 2 性能
+
+以MuseTalk仓内data/video/sun.mp4和data/audio/sun.wav为例，得到性能数据：
+
+   |模型|芯片|E2E|forward|
+   |------|------|------|---|
+   |MuseTalk|Atlas 300I DUO|234s|33s|
+
+   - forward包含vae和unet模型的耗时
+   - 将`config/inference/test.yaml`文件中的`task_0`注释掉
+
+### 3 FAQ
+1. 首次推理会报依赖权重错误，将其下载后放到对应路径即可。
+   ```
+   Downloading: "https://www.adrianbulat.com/downloads/python-fan/s3fd-619a316812.pth" to /root/.cache/torch/hub/checkpoints/s3fd-619a316812.pth
+   ```
+
---
+++ ACL_PyTorch/built-in/cv/MuseTalk/infer_npu.py
@@ -0,0 +1,253 @@
+# This file was copied and refactored from project https://github.com/TMElyralab/MuseTalk
+# Copyright (c) 2025 Huawei Technologies Co., Ltd
+# [Software Name] is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+
+import argparse
+import os
+import glob
+import pickle
+import copy
+import shutil
+import time
+import numpy as np
+import cv2
+import torch
+import torch_npu
+import torchair as tng
+
+from tqdm import tqdm
+from omegaconf import OmegaConf
+from torchair.configs.compiler_config import CompilerConfig
+from torch_npu.contrib import transfer_to_npu
+
+from musetalk.utils.utils import get_file_type, get_video_fps, datagen
+from musetalk.utils.preprocessing import get_landmark_and_bbox, read_imgs, coord_placeholder
+from musetalk.utils.blending import get_image
+from musetalk.utils.utils import load_all_model
+
+
+# load model weights
+audio_processor, vae, unet, pe = load_all_model()
+device = torch.device("npu" if torch.npu.is_available() else "cpu")
+timesteps = torch.tensor([0], device=device)
+
+
+def compile_model():
+    config = CompilerConfig()
+    config.experimental_config.frozen_parameter = True
+    npu_backend = tng.get_npu_backend(compiler_config=config)
+    models = [{"model": audio_processor.model.encoder, "dynamic": False},
+              {"model": vae.vae.encoder, "dynamic": False},
+              {"model": unet.model, "dynamic": True},
+              {"model": vae.vae.decoder, "dynamic": True}]
+    for model in models:
+        model["model"] = torch.compile(model["model"], dynamic=model["dynamic"], fullgraph=True, backend=npu_backend)
+        tng.use_internal_format_weight(model["model"])
+
+
+def extract_from_video_audio(video_path, audio_path, input_basename, args):
+    ############################################## extract frames from video ##############################################
+    if get_file_type(video_path) == "video":
+        save_dir_full = os.path.join(args.result_dir, input_basename)
+        os.makedirs(save_dir_full, exist_ok=True)
+        cmd = f"ffmpeg -v fatal -i {video_path} -start_number 0 {save_dir_full}/%08d.png"
+        if os.path.isfile(video_path) and os.path.isdir(save_dir_full):
+            os.system(cmd)
+        else:
+            raise ValueError(f"{video_path} should be a video file, {save_dir_full} should be a dir")
+        input_img_list = sorted(glob.glob(os.path.join(save_dir_full, '*.[jpJP][pnPN]*[gG]')))
+        fps = get_video_fps(video_path)
+    elif get_file_type(video_path) == "image":
+        input_img_list = [video_path, ]
+        fps = args.fps
+    elif os.path.isdir(video_path):  # input img folder
+        input_img_list = glob.glob(os.path.join(video_path, '*.[jpJP][pnPN]*[gG]'))
+        input_img_list = sorted(input_img_list, key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))
+        fps = args.fps
+    else:
+        raise ValueError(f"{video_path} should be a video file, an image file or a directory of images")
+    
+    ############################################## extract audio feature ##############################################
+    whisper_feature = audio_processor.audio2feat(audio_path)
+    whisper_chunks = audio_processor.feature2chunks(feature_array=whisper_feature, fps=fps)
+    
+    return input_img_list, whisper_chunks, fps
+
+
+def preprocess_input_image(input_img_list, bbox_shift, crop_coord_save_path, args):
+    ############################################## preprocess input image  ##############################################
+    if os.path.exists(crop_coord_save_path) and args.use_saved_coord:
+        print("using extracted coordinates")
+        with open(crop_coord_save_path, 'rb') as f:
+            coord_list = pickle.load(f)
+        frame_list = read_imgs(input_img_list)
+    else:
+        print("extracting landmarks...time consuming")
+        coord_list, frame_list = get_landmark_and_bbox(input_img_list, bbox_shift)
+        with open(crop_coord_save_path, 'wb') as f:
+            pickle.dump(coord_list, f)
+            
+    input_latent_list = []
+    for bbox, frame in zip(coord_list, frame_list):
+        if bbox == coord_placeholder:
+            continue
+        x1, y1, x2, y2 = bbox
+        crop_frame = frame[y1:y2, x1:x2]
+        crop_frame = cv2.resize(crop_frame, (256, 256), interpolation=cv2.INTER_LANCZOS4)
+        latents = vae.get_latents_for_unet(crop_frame)
+        input_latent_list.append(latents)
+    
+    # to smooth the first and the last frame
+    frame_list_cycle = frame_list + frame_list[::-1]
+    coord_list_cycle = coord_list + coord_list[::-1]
+    input_latent_list_cycle = input_latent_list + input_latent_list[::-1]
+
+    return frame_list_cycle, coord_list_cycle, input_latent_list_cycle
+
+
+def preprocess(inference_config, task_id, args):
+    video_path = inference_config[task_id]["video_path"]
+    audio_path = inference_config[task_id]["audio_path"]
+    bbox_shift = inference_config[task_id].get("bbox_shift", args.bbox_shift)
+
+    input_basename = os.path.basename(video_path).split('.')[0]
+    audio_basename = os.path.basename(audio_path).split('.')[0]
+    output_basename = f"{input_basename}_{audio_basename}"
+    result_img_save_path = os.path.join(args.result_dir, output_basename) # related to video & audio inputs
+    crop_coord_save_path = os.path.join(result_img_save_path, input_basename + ".pkl") # only related to video input
+    os.makedirs(result_img_save_path, exist_ok=True)
+    
+    if args.output_vid_name is None:
+        output_vid_name = os.path.join(args.result_dir, output_basename + ".mp4")
+    else:
+        output_vid_name = os.path.join(args.result_dir, args.output_vid_name)
+    
+    input_img_list, whisper_chunks, fps = extract_from_video_audio(video_path, audio_path, input_basename, args)
+    
+    frame_list_cycle, coord_list_cycle, input_latent_list_cycle = \
+        preprocess_input_image(input_img_list, bbox_shift, crop_coord_save_path, args)
+
+    pd = {
+        "frame_list_cycle": frame_list_cycle,
+        "coord_list_cycle": coord_list_cycle,
+        "input_latent_list_cycle": input_latent_list_cycle,
+        "whisper_chunks": whisper_chunks,
+        "output_vid_name": output_vid_name,
+        "result_img_save_path": result_img_save_path,
+        "audio_path": audio_path,
+        "fps": fps
+    }
+    return pd
+
+
+def postprocess(res_frame_list, postparm):
+    ############################################## pad to full image ##############################################
+    print("pad talking image to original video")
+    frame_list_cycle = postparm.get("frame_list_cycle")
+    coord_list_cycle = postparm.get("coord_list_cycle")
+    output_vid_name = postparm.get("output_vid_name")
+    result_img_save_path = postparm.get("result_img_save_path")
+    audio_path = postparm.get("audio_path")
+    fps = postparm.get("fps")
+    for i, res_frame in enumerate(tqdm(res_frame_list)):
+        bbox = coord_list_cycle[i % (len(coord_list_cycle))]
+        ori_frame = copy.deepcopy(frame_list_cycle[i % (len(frame_list_cycle))])
+        x1, y1, x2, y2 = bbox
+        try:
+            res_frame = cv2.resize(res_frame.astype(np.uint8), (x2 - x1, y2 - y1))
+        except Exception as ep:
+            print(f"Exception: {ep}, error in resize the {i}'th res_frame, bbox: {bbox}")
+            continue
+        
+        combine_frame = get_image(ori_frame, res_frame, bbox)
+        cv2.imwrite(f"{result_img_save_path}/{str(i).zfill(8)}.png", combine_frame)
+
+    cmd_img2video = f"ffmpeg -y -v warning -r {fps} -f image2 -i {result_img_save_path}/%08d.png -vcodec libx264 -vf format=rgb24,scale=out_color_matrix=bt709,format=yuv420p -crf 18 temp.mp4"
+    print(cmd_img2video)
+    os.system(cmd_img2video)
+    
+    cmd_combine_audio = f"ffmpeg -y -v warning -i {audio_path} -i temp.mp4 {output_vid_name}"
+    print(cmd_combine_audio)
+    os.system(cmd_combine_audio)
+    
+    os.remove("temp.mp4")
+    shutil.rmtree(result_img_save_path)
+    print(f"result is save to {output_vid_name}")
+
+
+def infer(input_latent_list_cycle, whisper_chunks):
+    ############################################## inference batch by batch ##############################################
+    video_num = len(whisper_chunks)
+    batch_size = args.batch_size
+    gen = datagen(whisper_chunks, input_latent_list_cycle, batch_size)
+    res_frame_list = []
+    for _, (whisper_batch, latent_batch) in enumerate(tqdm(gen, total=int(np.ceil(float(video_num) / batch_size)))):
+        audio_feature_batch = torch.from_numpy(whisper_batch)
+        audio_feature_batch = audio_feature_batch.to(device=unet.device,
+                                                        dtype=unet.model.dtype) # torch, B, 5*N,384
+        audio_feature_batch = pe(audio_feature_batch)
+        latent_batch = latent_batch.to(dtype=unet.model.dtype)
+        
+        pred_latents = unet.model(latent_batch, timesteps, encoder_hidden_states=audio_feature_batch, return_dict=False)[0]
+        recon = vae.decode_latents(pred_latents)
+        for res_frame in recon:
+            res_frame_list.append(res_frame)
+
+    return res_frame_list
+
+
+def main(args):
+    inference_config = OmegaConf.load(args.inference_config)
+    print(inference_config)
+    for task_id in inference_config:
+        pd = preprocess(inference_config, task_id, args)
+
+        res_frame_list = infer(pd.get("input_latent_list_cycle"), pd.get("whisper_chunks"))
+
+        postprocess(res_frame_list, pd)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inference_config", type=str, default="configs/inference/test_img.yaml")
+    parser.add_argument("--bbox_shift", type=int, default=0)
+    parser.add_argument("--result_dir", default='./results', help="path to output")
+
+    parser.add_argument("--fps", type=int, default=25)
+    parser.add_argument('--warmup', type=int, default=2, help="Warm up times")
+    parser.add_argument('--loop', type=int, default=3, help="loop times")
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--output_vid_name", type=str, default=None)
+    parser.add_argument("--use_saved_coord",
+                        action="store_true",
+                        help='use saved coordinate to save time')
+    parser.add_argument("--use_float16",
+                        action="store_true",
+                        help="Whether use float16 to speed up inference",
+    )
+
+    args = parser.parse_args()
+    if args.use_float16 is True:
+        pe = pe.half()
+        vae.vae = vae.vae.half()
+        unet.model = unet.model.half()
+    compile_model()
+    
+    # warmup
+    for _ in range(args.warmup):
+        with torch.no_grad():
+            main(args)
+
+    start = time.time()
+    for _ in range(args.loop):
+        with torch.no_grad():
+            main(args)
+    print(f'E2E time = {(time.time() - start) / args.loop *1000}ms')
