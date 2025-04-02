82337b5199297d26442e08c73a68beb0c20888d8
yliuls <liuyaofu2@h-partners.com>
2025-04-02 02:09:10 +0000

    !7148 删除modelzoo 上的opensoraplan1.3的代码。 最新的代码在魔乐上。现在的版本不能正常运行 Merge pull request !7148 from yliuls/master

--- MindIE/MultiModal/OpenSoraPlan-1.3/layers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .wavelet import (HaarWaveletTransform2D, HaarWaveletTransform3D, 
-                      InverseHaarWaveletTransform2D, InverseHaarWaveletTransform3D)
-from .conv import AttnBlock3DFix
-from .mlp import Mlp
-from .norm import AdaLayerNorm, VideoLayerNorm, Normalize
-from .sampling import Upsample, Downsample, Spatial2xTime2x3DDownsample, Spatial2xTime2x3DUpsample
