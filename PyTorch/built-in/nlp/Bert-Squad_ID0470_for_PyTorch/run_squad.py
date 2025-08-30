d8a08e8d069556c3e87319e0c48077b53c32d177
王凯宇 <wangkaiyu11@h-partners.com>
2025-08-30 07:07:43 +0000

    !7364 [built-in][nlp][Bert-Squad-ID0470_for_PyTorch]close internal_format to avoid Nz error Merge pull request !7364 from 王凯宇/master

--- PyTorch/built-in/nlp/Bert-Squad_ID0470_for_PyTorch/run_squad.py
+++ PyTorch/built-in/nlp/Bert-Squad_ID0470_for_PyTorch/run_squad.py
@@ -31,7 +31,7 @@ import numpy as np
 import torch
 if torch.__version__ >= "1.8":
     import torch_npu
-    torch.npu.config.allow_internal_format = True
+    torch.npu.config.allow_internal_format = False
 from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
                               TensorDataset)
 from torch.utils.data.distributed import DistributedSampler
