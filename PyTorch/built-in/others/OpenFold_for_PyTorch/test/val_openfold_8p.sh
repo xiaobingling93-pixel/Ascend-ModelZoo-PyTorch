dc949e4f4f29bc532e793478222dc839c3a2e078
王凯宇 <wangkaiyu11@h-partners.com>
2025-09-18 02:14:12 +0000

    !7391 update PyTorch/built-in/others/OpenFold_for_PyTorch/test/val_openfold_8p.sh Merge pull request !7391 from 王凯宇/master

--- PyTorch/built-in/others/OpenFold_for_PyTorch/test/val_openfold_8p.sh
+++ PyTorch/built-in/others/OpenFold_for_PyTorch/test/val_openfold_8p.sh
@@ -45,7 +45,7 @@ python3 train_openfold.py  $data_path/pdb_data/mmcif_files \
 wait
 
 # 提取最后一行中的 val/loss 小数值并赋值给变量
-val_loss=$(grep 'val/loss' openfold_val_8p.log | tail -n1 | awk '{print $(NF-1)}')
+val_loss=$(grep 'val/loss' openfold_val_8p.log | tail -n1 | awk '{print $(NF)}')
 
 # 输出结果
 echo "val/loss: $val_loss"
