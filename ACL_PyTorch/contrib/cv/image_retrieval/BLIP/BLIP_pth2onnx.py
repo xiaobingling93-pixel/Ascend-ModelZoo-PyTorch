aaaf8edf35087f59de1cdfd2d50d0738baaeaeb0
frost_mourne <suyuxuan3@h-partners.com>
2025-11-25 14:34:36 +0800

    添加精度评估功能
    Co-authored-by: frost_mourne<suyuxuan3@h-partners.com>

--- ACL_PyTorch/contrib/cv/image_retrieval/BLIP/BLIP_pth2onnx.py
+++ ACL_PyTorch/contrib/cv/image_retrieval/BLIP/BLIP_pth2onnx.py
@@ -1,41 +1,13 @@
-# BSD 3-Clause License
-#
-# Copyright (c) 2017 xxxx
-# All rights reserved.
-# Copyright 2021 Huawei Technologies Co., Ltd
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# * Neither the name of the copyright holder nor the names of its
-#   contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# ============================================================================
-
-# coding:utf-8
+# Copyright (c) OpenMMLab. All rights reserved.
+# Copyright (c) 2025 Huawei Technologies Co., Ltd
+# This software is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 
 import argparse
-import torch
-import onnx
-from transformers import BertTokenizer
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -127,8 +99,6 @@ class BLIP_ITM_IMAGE(nn.Module):
 
     def forward(self, image):
         image_embeds = self.visual_encoder(image)
-        image_atts = torch.ones(
-            image_embeds.size()[:-1], dtype=torch.long).to(image.device)
         image_feat = F.normalize(
             self.vision_proj(image_embeds[:, 0, :]), dim=-1)
         return image_feat
@@ -197,13 +167,12 @@ def pth2onnx_text(input_file, output_file, device='cpu'):
     model.eval()
     model.to(device)
 
-    input_image = torch.randn(1, 3, 384, 384)
     input_caption = ['Here is a apple']
 
     text = tokenizer(input_caption, padding='max_length',
                      truncation=True, max_length=35, return_tensors="pt")
-    text_ids = text.input_ids
-    text_atten_mask = text.attention_mask
+    text_ids = text.input_ids.to(torch.int32)
+    text_atten_mask = text.attention_mask.to(torch.int32)
 
     dummy_input = (text_ids, text_atten_mask)
 
